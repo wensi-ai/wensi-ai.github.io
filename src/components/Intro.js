c28c4db225fb08f897e2523fdc0297cfa3c4c87c
Tristan816la <tristanq816@gmail.com>
2022-11-30 20:53:05 -0800

    position relative replaced + components layout

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import { name, long_descroption, contacts, tech_stack } from '../data/Info'
-import "../styles/Home.css"
+import React from "react";
+import { name, long_descroption, contacts, tech_stack } from "../data/Info";
+import "../styles/Home.css";
 
 export default function About() {
-    return (
-        <div className='Intro'>
-            <div className="IntroWrapper">
-                <main className="About">
-                    <h1>Hi, I'm {name}</h1>
-                    <div className='IntroParagraph'>{long_descroption}</div>
-                    <div className='ContactImg'>
-                        {
-                            contacts.map(function(dict){
-                                return <a href={dict.url} key={dict.name}><img src={dict.img} alt={dict.name}/></a>
-                            }) 
-                        }
-                    </div>
-                </main>
-                <img src='/selfie.jpg' alt='selfie' />
-            </div>
-            <br />
+  return (
+    <div className="Intro">
+      <div className="IntroWrapper">
+        <h1>Hi, I'm {name}</h1>
+        <main className="About">
+          <div className="IntroParagraph">{long_descroption}</div>
+          <img src="/selfie.jpg" alt="selfie" />
+        </main>
+
+        <div className="ContactImg">
+          {contacts.map(function (dict) {
+            return (
+              <a href={dict.url} key={dict.name}>
+                <img src={dict.img} alt={dict.name} />
+              </a>
+            );
+          })}
         </div>
-    )
+      </div>
+      <br />
+    </div>
+  );
 }
