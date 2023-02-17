5757da67c0721642aa96b15bce898039e357ac48
Vince Ai <vinceai@outlook.com>
2023-02-17 10:39:57 -0800

    Fix typo in intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -20,7 +20,7 @@ export default function About() {
                     <div className="AboutParagraph">
                         I am Wensi (Vince) Ai, a senior CS & Applied Mathematics undergraduate at UCLA. 
                         I am currently working as a research assistant under Prof. <a href="https://jiajunwu.com">Jiajun Wu</a> at <a href="https://svl.stanford.edu">Stanford Vision and Learning Lab (SVL)</a>.
-                        Previously I worked as an undergraduate research assistant under Prof. <a href="http://www.stat.ucla.edu/~sczhu/">Song-Chun Wu</a> and Prof. <a href="http://www.stat.ucla.edu/~ywu/">Ying Nian Wu</a> at <a href="https://vcla.stat.ucla.edu/">Center for Vision, Cognition, Learning, and Autonomy (VCLA)</a>.
+                        Previously I worked as an undergraduate research assistant under Prof. <a href="http://www.stat.ucla.edu/~sczhu/">Song-Chun Zhu</a> and Prof. <a href="http://www.stat.ucla.edu/~ywu/">Ying Nian Wu</a> at <a href="https://vcla.stat.ucla.edu/">Center for Vision, Cognition, Learning, and Autonomy (VCLA)</a>.
                         I've also been fortunate to work as a SWE intern at <a href="https://www.metabit-trading.com">Metabit Trading</a>. 
                         I am broadly interested in computer graphics, vision, and robotics, with a special interest in {research_interest}.
                         Besides academics, I also love to do all kinds of activities, including running, hiking, cycling, and playing the violin.
